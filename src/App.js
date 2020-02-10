4ec161186b5f6123be32b09f775507e7ac603b00
Clem <clemence.fauchet23@gmail.com>
2020-02-10 17:53:12 +0100

    Creating question & answer components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import './App.scss'
+import Question from './components/Question'
 
 function App() {
-  return <div className='App'>Code goes here</div>
+  return (
+    <div className='App'>
+      <div className='App-header'>
+        <h2>React Quiz</h2>
+      </div>
+      <Question content='What is your favorite food?' />
+    </div>
+  )
 }
 
 export default App
